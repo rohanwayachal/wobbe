ed8212f342820a3443c3e05badc9d88c2a0f6529
rohanwayachal <ronwayachal@gmail.com>
2019-08-27 01:15:45 -0700

    Updates

---
+++ precache-manifest.e03519273232c49aeb1b354a440b0b28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0edd914bcfb8c255724c0d3777aef1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e9ae5511f1187403eff",
+    "url": "/static/css/main.170d5f63.chunk.css"
+  },
+  {
+    "revision": "59454e8bbc8fafcef718",
+    "url": "/static/js/2.b6b3773c.chunk.js"
+  },
+  {
+    "revision": "1e9ae5511f1187403eff",
+    "url": "/static/js/main.36da4a8d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
